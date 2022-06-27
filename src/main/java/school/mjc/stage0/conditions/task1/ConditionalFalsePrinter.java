13751827ad6045efc7d21f42e0fcf49aff8b0ebd
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-27 17:12:26 +0300

    Update ConditionalFalsePrinter.java

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,7 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
-            if(true)
+    if(false)
       System.out.println("true");
+      else
+      System.out.println("false");
     }
 }
